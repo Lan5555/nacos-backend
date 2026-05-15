cb33b91aebace49ff04d4e7827809f079a958886
Lan5555 <okekejohnson24@gmail.com>
2026-05-15 03:59:36 +0100

    Update

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user-entities';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { MailService } from 'src/workers/email-service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService],
+  providers: [UsersService, MailService],
   controllers: [UsersController],
 })
 export class UsersModule {}
