8553d27a95c1585bba52b8d65e94c78e2d90cd71
Lan5555 <okekejohnson24@gmail.com>
2026-05-16 18:27:14 +0100

    update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @Post('login-admin')
+  async loginAdmin(email: string, password: string) {
+    return await this.authService.loginAdmin(email, password);
+  }
+  @Post('login-user')
+  async loginUser(email: string, password: string) {
+    return await this.authService.loginUser(email, password);
+  }
 }
