10d618397614409607558f02c2b86af4ff3e5d43
Lan5555 <okekejohnson24@gmail.com>
2026-05-16 13:25:25 +0100

    update

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -6,12 +6,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/users/users.module';
 import { NotificationsModule } from 'src/notifications/notifications.module';
 import { User } from 'src/users/entities/user-entities';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Admin, User]),
     UsersModule,
     NotificationsModule,
+    CloudinaryModule,
   ],
   controllers: [AdminController],
   providers: [AdminService],
