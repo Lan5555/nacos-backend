1d873183b4419e3d04e4f88d906cd7f2936a2c07
Lan5555 <okekejohnson24@gmail.com>
2026-05-16 07:13:15 +0100

    update

--- src/cloudinary/cloudinary.controller.ts
+++ src/cloudinary/cloudinary.controller.ts
@@ -8,6 +8,8 @@ import {
 } from '@nestjs/common';
 import { CloudinaryService } from './cloudinary.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import type { Future } from 'src/helpers/helpers';
+import type { Express } from 'express';
 
 @Controller('cloud')
 export class CloudinaryController {
@@ -18,7 +20,7 @@ export class CloudinaryController {
   async uploadFile(
     @UploadedFile() file: Express.Multer.File,
     @Query('folder') folder?: string,
-  ) {
+  ): Future {
     return this.cloudinaryService.uploadFile(file, folder);
   }
 
@@ -26,20 +28,20 @@ export class CloudinaryController {
   async getDownloadUrl(
     @Query('publicId') publicId: string,
     @Query('resourceType') resourceType?: string,
-  ) {
+  ): Future {
     return this.cloudinaryService.getDownloadUrl(publicId, resourceType);
   }
 
   @Post('delete')
-  async deleteFile(@Query('publicId') publicId: string) {
+  async deleteFile(@Query('publicId') publicId: string): Future {
     return this.cloudinaryService.deleteFile(publicId);
   }
   @Get('get')
-  async getFile(@Query('publicId') publicId: string) {
+  async getFile(@Query('publicId') publicId: string): Future {
     return this.cloudinaryService.getFile(publicId);
   }
   @Get('list')
-  async listFiles(@Query('publicId') publicId?: string) {
+  async listFiles(@Query('publicId') publicId?: string): Future {
     return this.cloudinaryService.listFiles(publicId);
   }
 }
--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -3,11 +3,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CloudinaryService } from './cloudinary.service';
 import { CloudinaryController } from './cloudinary.controller';
 import { CloudinaryFile } from './entities/cloudinary-file.entity';
+import { v2 as cloudinary } from 'cloudinary';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CloudinaryFile])],
   controllers: [CloudinaryController],
-  providers: [CloudinaryService],
+  providers: [
+    CloudinaryService,
+    {
+      provide: 'CLOUDINARY',
+      useFactory: () => {
+        return cloudinary.config({
+          cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+          api_key: process.env.CLOUDINARY_API_KEY,
+          api_secret: process.env.CLOUDINARY_API_SECRET,
+        });
+      },
+    },
+  ],
   exports: [CloudinaryService],
 })
 export class CloudinaryModule {}
