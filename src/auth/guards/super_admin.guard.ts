d04eaabb6c849b4e64bd0a45e5f89f0d3fc0c62a
Lan5555 <okekejohnson24@gmail.com>
2026-05-19 13:06:42 +0100

    update

--- src/auth/guards/super_admin.guard.ts
+++ src/auth/guards/super_admin.guard.ts
@@ -1,21 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import {
+  CanActivate,
   ExecutionContext,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
 
 @Injectable()
-class SuperAdminGuard {
+class SuperAdminGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const req = context.switchToHttp().getRequest();
+
     if (!req.user) {
       throw new UnauthorizedException('Not logged in');
     }
-    if (req.role != 'admin' && req.user.adminLevel !== 2) {
+
+    if (req.user.role !== 'admin' || req.user.adminLevel !== 2) {
       throw new UnauthorizedException('Super admins only');
     }
+
     return true;
   }
 }
--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -6,15 +6,19 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { ContactService } from './contact.service';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
+import { SuperAdminGuard } from 'src/auth/guards/super_admin.guard';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('contact')
 export class ContactController {
   constructor(private readonly contactService: ContactService) {}
 
+  @UseGuards(AuthGuard('jwt'), SuperAdminGuard)
   @Post('save-contact')
   create(@Body() createContactDto: CreateContactDto) {
     return this.contactService.create(createContactDto);
@@ -29,12 +33,12 @@ export class ContactController {
   findOne(@Param('id') id: string) {
     return this.contactService.findOne(+id);
   }
-
+  @UseGuards(AuthGuard('jwt'), SuperAdminGuard)
   @Patch('update-contact/:id')
   update(@Param('id') id: string, @Body() updateContactDto: UpdateContactDto) {
     return this.contactService.update(+id, updateContactDto);
   }
-
+  @UseGuards(AuthGuard('jwt'), SuperAdminGuard)
   @Delete('delete-contact/:id')
   remove(@Param('id') id: string) {
     return this.contactService.remove(+id);
